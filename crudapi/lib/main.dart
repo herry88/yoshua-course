85d92f80678d08972197b8891c1aee00958cb43a
herry88 <herry081288@gmail.com>
2021-11-12 18:43:40 +0700

    addprojectAPi

---
+++ crudapi/lib/main.dart
@@ -0,0 +1,82 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'My Store',
+      home: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Future<List>? getData() async {
+    final response = await http.get(
+      Uri.parse(
+        "https://herryprasetyo.my.id/latihan/get_data.php",
+      ),
+    );
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "MyStore",
+        ),
+      ),
+      body: FutureBuilder<List>(
+        future: getData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) print(snapshot.error);
+          return snapshot.hasData
+              ? ItemList(list: snapshot.data)
+              : const Center(
+                  child: Text('Gagal Ambil Data Bro'),
+                );
+        },
+      ),
+    );
+  }
+}
+
+class ItemList extends StatelessWidget {
+  List? list;
+  ItemList({Key? key, this.list}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: list == null ? 0 : list!.length,
+      itemBuilder: (context, i) {
+        return Container(
+          child: Card(
+            child: ListTile(
+              title: Text(
+                list![i]['item_name'],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
