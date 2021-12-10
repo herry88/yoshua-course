b582fb624e9ccd69f7671aeb6dc86a8fcd38b02d
Herry Prasetyo <herry081288@gmail.com>
2021-12-10 19:33:38 +0700

    homePage

--- newsappsflutter/lib/views/homepage.dart
+++ newsappsflutter/lib/views/homepage.dart
@@ -1,7 +1,7 @@
-import 'dart:html';
-
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:newsappsflutter/helper/widgets.dart';
+import 'package:newsappsflutter/models/category.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,6 +11,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // bool loading;
+  var newsList;
+  List<CategoryModel> categories = <CategoryModel>[];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,7 +41,62 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.white,
         elevation: 0.0,
       ),
-      body: Center(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+              child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                ),
+                height: 70,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: categories.length,
+                  itemBuilder: (context, index) {
+                    return CategoryCard(
+                      imageAssetUrl: categories[index].imageAssetUrl,
+                      categoryName: categories[index].categoryName,
+                    );
+                  },
+                ),
+              ),
+            ],
+          )),
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  String? imageAssetUrl, categoryName;
+  CategoryCard({Key? key, this.imageAssetUrl, this.categoryName})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        margin: const EdgeInsets.only(
+          right: 14.0,
+        ),
+        child: Stack(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(5.0),
+              child: CachedNetworkImage(
+                imageUrl: imageAssetUrl.toString(),
+                height: 60,
+                width: 120.0,
+                fit: BoxFit.cover,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
